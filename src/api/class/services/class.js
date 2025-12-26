9e0a63ff20bc440696890d154d23b28ab714e667
Israel Albarrán <israelalbarran@Israels-MacBook-Air.local>
2025-12-25 22:20:12 -0600

    feat(strapi): add Class content type for MindCo

--- src/api/class/services/class.js
+++ src/api/class/services/class.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * article service.
+ * class service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::article.article');
+module.exports = createCoreService('api::class.class');
